1460824a3dd815d05f7b1504595b0bf601b3af2b
Sachin Ghuge <ghugesachin4545@gmail.com>
2025-05-06 19:29:52 +0530

    initial commit

---
+++ src/main/java/com/qnahub/qnabackend/QnabackendApplication.java
@@ -0,0 +1,13 @@
+package com.qnahub.qnabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnahub/qnabackend/QnabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnahub.qnabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
